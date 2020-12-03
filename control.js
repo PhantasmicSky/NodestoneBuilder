279e6417edde3df0645de0bebbc903098f96a627
PhantasmicSky <blake010899@gmail.com>
2020-12-03 18:53:18 +0800

    Added the sorting function to current nodes created, node stone trios, and NodeStatistics2

--- control.js
+++ control.js
@@ -481,6 +481,21 @@ function clearLeftoverData(){
 
 }
 
+$('table[name="sortable"] th').click(function(){
+    var table = $(this).parents('table').eq(0)
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+    this.asc = !this.asc
+    if (!this.asc){rows = rows.reverse()}
+    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+})
+function comparer(index) {
+    return function(a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index)
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+    }
+}
+function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
+
 
 function checkVar(){
     console.log(nodestones);
